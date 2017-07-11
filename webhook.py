5e03e6e82247ad05d8434e84e0c0a2cf241e5a4b
NishaNarayanaswamy <nisha.narayanaswamy@sikkasoftware.com>
2017-07-11 10:03:10 -0700

    Add files via upload

---
+++ webhook.py
@@ -0,0 +1,25 @@
+import logging
+from flask import Flask
+from flask_assistant import Assistant, ask, tell, context_manager
+
+app = Flask(__name__)
+assist = Assistant(app)
+logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+
+session_filename = 'user_sess.txt'
+file = open(session_filename, 'a')
+
+@assist.action('capture')  # specify intent name
+def capture_note(note):
+    file.write(str(note))
+    speech = "you said... "+note+' \nAnything else to add?'
+    return ask(speech)
+
+@assist.action('appendNote')  # specify intent name
+def capture_note(appendNote):
+    file.write(str(appendNote))
+    speech = "added note... "+appendNote
+    return ask(speech)
+
+if __name__ == '__main__':
+    app.run(debug=True)
